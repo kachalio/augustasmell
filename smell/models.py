45ff9524baf5b53c26781a67754c3c3433951334
cjhelms <cjhelms1428@gmail.com>
2020-06-14 23:43:13 -0400

    updated database & front end to include basic rating system

--- smell/models.py
+++ smell/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Smell(models.Model):
     smell = models.CharField(max_length=50)
     updated_datetime = models.DateTimeField()
+    rating = models.PositiveIntegerField()
 
     def __str__(self):
     	return self.smell
