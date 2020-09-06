aa9f6e214691e8e8b29434d00973cef0466a493f
kachalio <znkachaylo@gmail.com>
2020-09-06 19:09:48 -0400

    Added environment variables

--- augustasmell/settings.py
+++ augustasmell/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-import os
+import os, environ
+
+# Environment variables
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,10 +80,18 @@ WSGI_APPLICATION = 'augustasmell.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+            'ENGINE': env("ENGINE"),
+            'NAME': env("NAME"),
+            'USER': env("USER"),
+            'PASSWORD': env("PASSWORD"),
+            'HOST': env("HOST"),
+            'PORT': env("PORT"),
+        }
 }
 
 
